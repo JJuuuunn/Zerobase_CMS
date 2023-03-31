9155864ff4dec76bf0968d5ac39c663ad236e2ca
jjuuuunn <didtdjwns111@gmail.com>
2023-03-31 19:39:29 +0900

    feat : [003-EMAIL-VERIFICATION] 이메일 인증 간련 서비스 구현

--- user-api/src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
@@ -3,8 +3,15 @@ package com.zerobase.cms.user.service;
 import com.zerobase.cms.user.domain.SignUpForm;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.domain.repository.CustomerRepository;
+import com.zerobase.cms.user.exception.CustomException;
+import com.zerobase.cms.user.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +21,35 @@ public class SignUpCustomerService {
 
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
+    }
+
+    public boolean isEmailExist(String email) {
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
+                .isPresent();
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(customerId);
+        if (optionalCustomer.isPresent()) {
+            Customer c = optionalCustomer.get();
+            c.setVerificationCode(verificationCode, LocalDateTime.now().plusDays(1));
+            return c.getVerifyExpiredAt();
+        }
+        throw new CustomException(ErrorCode.NOT_FOUND_USER);
+    }
+
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer c = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+        if (c.isVerify()) {
+            throw new CustomException(ErrorCode.ALREADY_VERIFY);
+        } else if (!c.getVerificationCode().equals(code)) {
+            throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+        } else if (c.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(ErrorCode.EXPIRE_CODE);
+        }
+        c.verificationSuccess(true);
     }
 }
