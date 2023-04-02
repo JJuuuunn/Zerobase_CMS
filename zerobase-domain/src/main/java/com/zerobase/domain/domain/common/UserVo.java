ad05f161daad6a159e78af85356b10384506176f
jjuuuunn <didtdjwns111@gmail.com>
2023-04-02 15:06:12 +0900

    feat : [004-LOGIN-CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

---
+++ zerobase-domain/src/main/java/com/zerobase/domain/domain/common/UserVo.java
@@ -0,0 +1,14 @@
+package com.zerobase.domain.domain.common;
+
+import lombok.Getter;
+
+@Getter
+public class UserVo {
+    private Long id;
+    private String email;
+
+    public UserVo(Long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+}
